e834c32e3258b1153a8c5f4127db8d1a28ccb07e
tanvung <lientanvung2101@gmail.com>
2026-01-16 15:33:58 +0700

    update payment-api-key.guard

--- src/shared/guards/payment-api-key.guard.ts
+++ src/shared/guards/payment-api-key.guard.ts
@@ -5,7 +5,8 @@ import envConfig from 'src/shared/config'
 export class PaymentAPIKeyGuard  implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest()
-    const paymentApiKey  = request.headers['Authorization']?.split(' ')[1]
+    const paymentApiKey = request.headers['authorization']?.split(' ')[1]
+    console.log(request.headers['authorization'])
     if (paymentApiKey !== envConfig.PAYMENT_API_KEY) {
       throw new UnauthorizedException()
     }
